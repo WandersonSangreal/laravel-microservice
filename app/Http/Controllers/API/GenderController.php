5b653d567bb2a82641e922b63bede1b727739be8
Wanderson Sangreal <sangreal.wanderson@hotmail.com>
2021-05-30 16:18:20 -0300

    Arquivos que não foram acrescentados

---
+++ app/Http/Controllers/API/GenderController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Gender;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
+
+class GenderController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean',
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(Gender::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $serialized = $request->only(array_keys($this->rules));
+
+        $this->validate($request, $this->rules);
+
+        return response()->json(Gender::create($serialized));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Gender $gender
+     * @return JsonResponse
+     */
+    public function show(Gender $gender): JsonResponse
+    {
+        return response()->json($gender);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Gender $gender
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function update(Request $request, Gender $gender): JsonResponse
+    {
+        $serialized = $request->only(array_keys($this->rules));
+
+        $this->validate($request, $this->rules);
+
+        $gender->update($serialized);
+
+        return response()->json($gender);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Gender $gender
+     * @return JsonResponse
+     */
+    public function destroy(Gender $gender): JsonResponse
+    {
+        $gender->delete();
+
+        return response()->json([], 204);
+    }
+}
---
+++ app/Models/Gender.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gender extends Model
+{
+    use Uuid;
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $casts = ['id' => 'string'];
+    protected $fillable = ['name', 'is_active'];
+
+}
